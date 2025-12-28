6f6ef7bdd0c47334e221db30831a29a56999ffa8
Mariofhdez <mario.florez249@gmail.com>
2025-12-28 16:34:31 -0500

    Part3: exercise 3.15

--- index.js
+++ index.js
@@ -71,9 +71,10 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-    res.status(204).end()
+    const id = req.params.id
+    Person.findByIdAndDelete(id).then(result => {
+        res.status(204).end()
+    })
 })
 
 
